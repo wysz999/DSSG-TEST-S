3b3f4d43f4a4f930272a12974f6c01b59befecdf
wysz999 <1300145437@qq.com>
2016-06-16 15:16:12 +0800

    updata

---
+++ XMLDataManager.h
@@ -0,0 +1,23 @@
+#ifndef _XMLDATAMANAGER_H_
+#define _XMLDATAMANAGER_H_
+/**********************
+
+XML数据管理类
+
+**********************/
+#include "common\common.h"
+
+class XMLDataManager
+{
+public:
+	XMLDataManager();
+	~XMLDataManager();
+
+private:
+	bool LoadServerConfig();
+	bool ReLoadServerConfig();
+};
+
+
+
+#endif // !_XMLDATAMANAGER_H_
